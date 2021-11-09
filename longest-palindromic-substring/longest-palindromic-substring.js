c8b2a5ed7070c73ad90e2c76e61a6dafa1a16e39
Arpita <66761308+arpitapandya@users.noreply.github.com>
2021-11-09 14:48:48 -0500

    Time: 116 ms (73.74%), Space: 42 MB (59.67%) - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let res = "";
+    
+    if(!s) return res;
+    
+    for(let i = 0; i < s.length; i++) {
+        for(let j = 0; j < 2; j++) {
+            
+            let left = i;
+            let right = i + j;
+            
+            while(left >= 0 && right <= s.length && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            let start = left + 1;
+            let end = right;
+            
+            let subString = s.slice(start, end);
+            if(res.length < subString.length) {
+                res = subString
+            }
+        }
+    }
+    return res;
+};

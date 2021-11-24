c548e26b349e0bc7c16768cb427f1f9913c6e83b
Arpita <66761308+arpitapandya@users.noreply.github.com>
2021-11-24 00:03:16 -0500

    Time: 88 ms (60.08%), Space: 45.1 MB (46.96%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  let numbers = new Set();
+    
+    for (let num of nums) {
+        if (!numbers.has(num)) {
+            numbers.add(num);
+        } else {
+            return true;
+        }
+    }
+    return false;
+};

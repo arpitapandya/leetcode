9282decdbd090c81061852acd7bd7b829d51f4e2
Arpita <66761308+arpitapandya@users.noreply.github.com>
2022-01-08 20:43:56 -0500

    Time: 135 ms (32.91%), Space: 48.2 MB (21.72%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,10 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        for(let j = 0; j < i; j++) {
-            if(nums[i] == nums[j]) return true;
+    let numbers = new Set();
+    
+    for(let num of nums) {
+        if(!numbers.has(num)) {
+            numbers.add(num);
+        } else {
+            return true;
         }
     }
     return false;
-}
+};

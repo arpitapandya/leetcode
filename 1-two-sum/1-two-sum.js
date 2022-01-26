f7189de4949c802defae637939378a30cf5cfcd1
Arpita <66761308+arpitapandya@users.noreply.github.com>
2022-01-26 18:38:38 -0500

    Time: 118 ms (50.20%), Space: 40.4 MB (47.26%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,10 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let comp = {};
+    
     for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] == target)
-                return [i, j]
+            if(nums[i] in comp) {
+                return [comp[nums[i]], i];
+            }
+            comp[target - nums[i]] = i;
         }
-    }
+    return [];
 };
